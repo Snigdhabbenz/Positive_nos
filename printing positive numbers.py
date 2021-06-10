6086335be73682a9f1862b814d8097e9f48cb42a
Snigdhabbenz <82171018+Snigdhabbenz@users.noreply.github.com>
2021-06-10 15:57:20 +0530

    Add files via upload

---
+++ printing positive numbers.py
@@ -0,0 +1,14 @@
+#Printing positive numbers in a given range
+
+list1 = [12, -7, 5, 64, -14]
+print("List 1")
+for number in list1:
+    if number > 0:
+        print(number)
+print("\n")
+
+list2 = [12, 14, -95, 3]
+print("List 2")
+for number in list2:
+      if number > 0:
+          print(number)
